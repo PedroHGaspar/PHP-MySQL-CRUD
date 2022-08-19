c6a9977bae33e5f326399590c5b75c28a3f93d48
Pedro Henrique Gaspar de Figueiredo <ordep12@hotmail.com.br>
2022-08-19 12:42:19 -0300

    INT/VARCHAR/DATE/DATETIME/DECIMAL/LONGTEXT/BOOLEAN

---
+++ Informations about tables/informations.php
@@ -0,0 +1,17 @@
+Some relevant informations about tables:
+Algumas informações relevantes sobre tabelas:
+
+    
+    INT - números inteiros (0, 1, 2, 3, 4, -5, -10...)
+
+    VARCHAR - como se fosse string ("jasddajks sajkdj segyayse asyegye"). (Pode conter informações numéricas, como telefones, aliás, é bom para salvar telefones, pois o telefone em si é uma string: (48 9 9944-9944 > contém traço, espaço e parênteses)).
+
+    DATE - No mySQL ele faz y/m/d pois é o padrão americano.
+
+    DATETIME - Mesma coisa do DATE, porém com horário. (exemplo é a data e o horário que o cliente realizou uma compra pelo sistema ou se cadastrou no sistema)
+
+    DECIMAL - Números decimais, números quebrados. Você pode colocar a quantidade de números máximos que serão utilizados e também a quantidade de números máximos depois da vírgula. Exemplo: 10,2 - 5000,00 (aqui tem 4 números,2 números) 10,2 é muito bom e prático.
+
+    LONGTEXT - String sem tamanho definido. Bom para cases que terá um texto grande, como algum contato de suporte, algum texto grande do site ou coisas do tipo. (tamanho dinâmico)
+
+    BOOLEAN - true(1) ou false(0). Útil para campos onde a resposta será sim ou não.[ "O preço é " . (membroClube ? "R$50,00" : "R$100,00") ]
