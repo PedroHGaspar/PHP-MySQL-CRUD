0f216a5922c4ba4d1334b1945901cc99fd87590e
PedroHGaspar <ordep12@hotmail.com.br>
2022-08-29 10:48:44 -0300

    Read table e style

---
+++ Project/clientes.php
@@ -0,0 +1,51 @@
+<?php
+include('conexao.php');
+
+$sql_clientes = "SELECT * FROM clientes";
+$query_clientes = $mysqli->query($sql_clientes) or die($mysqli->error);
+$num_clientes = $query_clientes->num_rows;
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Clientes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+  <h1>Estes são os clientes cadastrados no seu sistema:</h1>
+  <div class="tbl-header">
+    <table cellpadding="0" cellspacing="0">
+      <thead>
+        <tr>
+          <th>Code</th>
+          <th>Company</th>
+          <th>Price</th>
+          <th>Change</th>
+          <th>Change %</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+  <div class="tbl-content">
+    <table cellpadding="0" cellspacing="0">
+      <tbody>
+        <tr>
+          <td>AAC</td>
+          <td>AUSTRALIAN COMPANY </td>
+          <td>$1.38</td>
+          <td>+2.01</td>
+          <td>-0.36%</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</section>
+</div>
+</body>
+</html>
