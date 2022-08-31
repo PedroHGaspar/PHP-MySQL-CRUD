fb09446c3239f5b3a3e283246bca730f9e59f10f
PedroHGaspar <ordep12@hotmail.com.br>
2022-08-30 23:35:54 -0300

    pagina inicial para quando tiver login vir pra ca 1º

---
+++ Project/pagina_inicial.php
@@ -0,0 +1,41 @@
+<?php
+include('conexao.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Clientes</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <section>
+        <div class="pagInicial">
+            <div>
+                <a href="clientes.php">Ver todos os clientes cadastrados no sistema.</a>
+            </div>
+            <div>
+                <a href="cadastrar_cliente.php">Adicionar um novo cliente ao sistema.</a>
+            </div>
+        </div>
+    </section>
+
+
+
+    </div>
+    <div class="msgBottom">
+        <span>Desenvolvido com amor por <a class="msgBottom" href="https://github.com/PedroHGaspar" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i>PedroHGaspar</a></span>
+    </div>
+            <!-- O atributo target definido como _blank, o que diz ao navegador para abrir o link em uma nova aba/janela, dependendo das configurações do navegador
+    
+            O atributo rel definido como noreferrer noopener para evitar possíveis ataques maliciosos das páginas as quais você fizer um vínculo -->
+</body>
+
+</html>
