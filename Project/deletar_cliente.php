71268c4d79c1f15c424f2b348658f6299bed1673
PedroHGaspar <ordep12@hotmail.com.br>
2022-08-30 23:19:03 -0300

    Deletar cliente funcionando. Tudo está ok! próximo passo: login

--- Project/deletar_cliente.php
+++ Project/deletar_cliente.php
@@ -9,8 +9,35 @@ if (isset($_POST['confirmar'])) { //Aqui o POST confirmar só vai existir se a p
 
     if ($sql_query) { ?>
 
-        <h1>Cliente deletado com sucesso.</h1>
-        <p><a href="clientes.php">Clique aqui para voltar para a lista de clientes.</a></p>
+        <!DOCTYPE html>
+        <html lang="pt-br">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="style.css">
+            <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <title>Cadastrar Cliente</title>
+        </head>
+
+        <body>
+            <form method="POST" action="">
+                <div class="content">
+                    <a href="clientes.php" style="font-size: 30px;">Voltar para a lista de clientes cadastrados.</a><br><br><br>
+                    <span class="msgDeletar">Cliente deletado com sucesso!</span>
+                </div>
+            </form>
+            <div class="msgBottom">
+                <span>Desenvolvido com amor por <a class="msgBottom" href="https://github.com/PedroHGaspar" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i>PedroHGaspar</a></span>
+            </div>
+            <!-- O atributo target definido como _blank, o que diz ao navegador para abrir o link em uma nova aba/janela, dependendo das configurações do navegador
+    
+            O atributo rel definido como noreferrer noopener para evitar possíveis ataques maliciosos das páginas as quais você fizer um vínculo -->
+        </body>
+
+        </html>
 
 <?php
         die();
